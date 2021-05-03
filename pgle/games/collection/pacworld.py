d7bc68da2a91c6fc664fcd912797beeb7ae5eb30
Sirui-Xu <xusirui@pku.edu.cn>
2021-05-03 20:57:32 +0800

    add pacworld

---
+++ pgle/games/collection/pacworld.py
@@ -0,0 +1,262 @@
+import pygame
+import sys
+sys.path.append("..")
+import math
+
+from base import PyGameWrapper, Player, Creep
+
+from utils import vec2d, percent_round_int
+from pygame.constants import K_w, K_a, K_s, K_d
+
+
+class PacWorld(PyGameWrapper):
+    """
+    The size of the node is related to the score. You should get a high score as soon as possible
+    Parameters
+    ----------
+    width : int
+        Screen width.
+    height : int
+        Screen height, recommended to be same dimension as width.
+    num_creeps : int (default: 3)
+        The number of creeps on the screen at once.
+    UNIFORM_SPEED : bool (default: false)
+        The agent has an uniform speed or not.
+    NO_SPEED : bool (default: false)
+        whether the node can move.
+    """
+
+    def __init__(self,
+                 width=48,
+                 height=48,
+                 num_creeps=3,
+                 UNIFORM_SPEED=False,
+                 NO_SPEED=False):
+
+        actions = {
+            "up": K_w,
+            "left": K_a,
+            "right": K_d,
+            "down": K_s
+        }
+        if min(width, height) < 50:
+            width = int(50 / min(width, height) * width) + 1
+            height = int(50 / min(width, height) * height) + 1
+        PyGameWrapper.__init__(self, width, height, actions=actions)
+        self.BG_COLOR = (255, 255, 255)
+        self.N_CREEPS = num_creeps
+        self.CREEP_TYPES = ["GOOD"]
+        self.CREEP_COLORS = [(40, 240, 40)]
+        radius = percent_round_int(min(width, height), 0.047)
+        self.CREEP_RADII = [percent_round_int(min(width, height), 0.020), percent_round_int(min(width, height), 0.080)]
+        self.CREEP_REWARD = [
+            self.rewards["positive"],
+            self.rewards["negative"]]
+        if NO_SPEED:
+            self.CREEP_SPEED = 0
+        else:
+            self.CREEP_SPEED = 0.5 * width
+        self.AGENT_COLOR = (30, 30, 30)
+        self.AGENT_SPEED = width
+        self.AGENT_RADIUS = radius
+        self.AGENT_INIT_POS = (self.width // 2, self.height // 2)
+        self.UNIFORM_SPEED = UNIFORM_SPEED
+        self.creep_counts = {
+            "GOOD": 0
+        }
+
+        self.dx = 0
+        self.dy = 0
+        self.player = None
+        self.creeps = None
+
+    def _handle_player_events(self):
+        self.dx = 0
+        self.dy = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                key = event.key
+
+                if key == self.actions["left"]:
+                    self.dx -= self.AGENT_SPEED
+
+                if key == self.actions["right"]:
+                    self.dx += self.AGENT_SPEED
+
+                if key == self.actions["up"]:
+                    self.dy -= self.AGENT_SPEED
+
+                if key == self.actions["down"]:
+                    self.dy += self.AGENT_SPEED
+
+    def _add_creep(self, creep_type, radius):
+        # creep_type = self.rng.choice([0, 1])
+
+        creep = None
+        pos = (0, 0)
+        dist = 0.0
+
+        while dist < self.AGENT_RADIUS + self.CREEP_RADII[-1] + 1:
+            d = self.CREEP_RADII[-1] * 1.5
+            pos = self.rng.uniform(d, self.height - d, size=2)
+            dist = math.sqrt(
+                (self.player.pos.x - pos[0])**2 + (self.player.pos.y - pos[1])**2)
+
+        creep = Creep(
+            self.CREEP_COLORS[creep_type],
+            radius,
+            pos,
+            self.rng.choice([-1, 1], 2),
+            self.rng.rand() * self.CREEP_SPEED,
+            self.CREEP_REWARD[creep_type] * radius,
+            self.CREEP_TYPES[creep_type],
+            self.width,
+            self.height,
+            self.rng.rand()
+        )
+
+        self.creeps.add(creep)
+
+        self.creep_counts[self.CREEP_TYPES[creep_type]] += 1
+
+    def getGameState(self):
+        """
+        Returns
+        -------
+        dict
+            * player x position.
+            * player y position.
+            * player x velocity.
+            * player y velocity.
+            * player distance to each creep
+        """
+        if self.UNIFORM_SPEED:
+            state = {
+                "player_x": self.player.pos.x,
+                "player_y": self.player.pos.y,
+                "creep_dist": {
+                    "GOOD": [],
+                    "BAD": []
+                },
+                "creep_pos": {
+                    "GOOD": [],
+                    "BAD": []
+                }
+            }
+        else:
+            state = {
+                "player_x": self.player.pos.x,
+                "player_y": self.player.pos.y,
+                "player_velocity_x": self.player.vel.x,
+                "player_velocity_y": self.player.vel.y,
+                "creep_dist": {
+                    "GOOD": [],
+                    "BAD": []
+                },
+                "creep_pos": {
+                    "GOOD": [],
+                    "BAD": []
+                }
+            }
+
+        for c in self.creeps:
+            dist = math.sqrt((self.player.pos.x - c.pos.x) **
+                             2 + (self.player.pos.y - c.pos.y)**2)
+            state["creep_dist"][c.TYPE].append(dist)
+            state["creep_pos"][c.TYPE].append([c.pos.x, c.pos.y])
+
+        return state
+
+    def getScore(self):
+        return self.score
+
+    def game_over(self):
+        """
+            Return bool if the game has 'finished'
+        """
+        return (self.creep_counts['GOOD'] == 0)
+
+    def init(self):
+        """
+            Starts/Resets the game to its inital state
+        """
+        self.creep_counts = {"GOOD": 0}
+        self.AGENT_INIT_POS = self.rng.uniform(self.AGENT_RADIUS, self.height - self.AGENT_RADIUS, size=2)
+
+        if self.player is None:
+            self.player = Player(
+                self.AGENT_RADIUS, self.AGENT_COLOR,
+                self.AGENT_SPEED, self.AGENT_INIT_POS,
+                self.width, self.height,
+                self.UNIFORM_SPEED
+            )
+
+        else:
+            self.player.pos = vec2d(self.AGENT_INIT_POS)
+            self.player.vel = vec2d((0.0, 0.0))
+            self.player.rect.center = self.AGENT_INIT_POS
+
+        if self.creeps is None:
+            self.creeps = pygame.sprite.Group()
+        else:
+            self.creeps.empty()
+
+        for i in range(self.N_CREEPS):
+
+            self._add_creep(0, self.rng.randint(self.CREEP_RADII[0], self.CREEP_RADII[1]))
+
+        self.score = 0
+        self.ticks = 0
+        self.lives = -1
+        self.screen.fill(self.BG_COLOR)
+        self.player.draw(self.screen)
+        self.creeps.draw(self.screen)
+
+
+    def step(self, dt):
+        """
+            Perform one step of game emulation.
+        """
+        dt /= 1000.0
+        self.screen.fill(self.BG_COLOR)
+
+        self.score += self.rewards["tick"]
+
+        self._handle_player_events()
+        self.player.update(self.dx, self.dy, dt)
+
+        hits = pygame.sprite.spritecollide(self.player, self.creeps, True)
+        for creep in hits:
+            self.creep_counts[creep.TYPE] -= 1
+            self.score += creep.reward
+            # self._add_creep(1)
+
+        if self.creep_counts["GOOD"] == 0:
+            self.score += self.rewards["win"]
+
+        self.creeps.update(dt)
+
+        self.player.draw(self.screen)
+        self.creeps.draw(self.screen)
+
+if __name__ == "__main__":
+    import numpy as np
+
+    pygame.init()
+    game = PacWorld(width=512, height=512, num_creeps=10)
+    game.screen = pygame.display.set_mode(game.getScreenDims(), 0, 32)
+    game.clock = pygame.time.Clock()
+    game.rng = np.random.RandomState(24)
+    game.init()
+
+    while True:
+        dt = game.clock.tick_busy_loop(30)
+        game.step(dt)
+        pygame.display.update()
+        if game.game_over() is True:
+            print("The overall score is {}.".format(game.score))
+            break
